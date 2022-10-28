e2081ff29858096b1f6a4d92a9b35c53592781b3
Rob Cavanaugh <46727239+rbcavanaugh@users.noreply.github.com>
2022-10-28 13:38:38 -0400

    comments

--- read-and-summarize.R
+++ read-and-summarize.R
@@ -13,8 +13,17 @@ files <- list.files(
 df <- files %>%
   map_dfr(read_csv, show_col_types = FALSE)
 
+# the final data is called summary_df
+# created by starting with df
+# then group by the variables of interest. 
+# I added condition, because it doesn't make a whole lot
+# of sense to drop it if you want to keep spt0217 column. 
+# Would be good to check and make sure the other columns dropped
+# arent important. 
 summary_df = df %>% 
-  group_by(participant,phase,session, condition, spt2017)%>%
+  # for each unique combo of these columns
+  group_by(participant,phase,session, condition, spt2017) %>%
+  # calculate mean accuracy, sum correct, and total trials
   summarize(accuracy=mean(response, na.rm = TRUE),
             sum_correct=sum(response, na.rm = TRUE),
             trials = n()) 
